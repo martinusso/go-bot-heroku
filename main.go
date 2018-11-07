57abbba0d57120c9028d9b3a01a9c0666b8d8021
Breno Martinusso <martinusso@gmail.com>
2018-11-07 15:26:23 -0200

    Add plugins

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"os"
 
 	"github.com/go-chat-bot/bot/rocket"
+	_ "github.com/go-chat-bot/plugins-br/cnpj"
+	_ "github.com/go-chat-bot/plugins-br/cpf"
+	_ "github.com/go-chat-bot/plugins-br/dilma"
+	_ "github.com/go-chat-bot/plugins-br/lula"
 	_ "github.com/go-chat-bot/plugins/catfacts"
 	_ "github.com/go-chat-bot/plugins/catgif"
 	_ "github.com/go-chat-bot/plugins/chucknorris"
+	_ "github.com/go-chat-bot/plugins/crypto"
+	_ "github.com/go-chat-bot/plugins/encoding"
 	_ "github.com/go-chat-bot/plugins/godoc"
+	_ "github.com/go-chat-bot/plugins/treta"
 )
 
 func main() {
